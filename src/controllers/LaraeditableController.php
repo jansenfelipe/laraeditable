c8f2b81f388260b5e556bb1d4704dba017a35204
Jansen Felipe <jansen.felipe@gmail.com>
2015-01-02 15:28:56 -0200

    Somente dom-crawler

--- src/controllers/LaraeditableController.php
+++ src/controllers/LaraeditableController.php
@@ -3,12 +3,20 @@
 namespace JansenFelipe\Laraeditable\Controllers;
 
 use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\Input;
+use Symfony\Component\DomCrawler\Crawler;
 
 class LaraeditableController extends Controller {
 
     public function postIndex() {
-        var_dump(Input::all());
+        $paths = Config::get('view.paths');
+        $view = $paths[0]  . DIRECTORY_SEPARATOR . Input::get('view'). '.blade.php';
+        
+        $crawler = new Crawler(file_get_contents($view));
+
+        
+        var_dump($crawler->filter(Input::get('element'))->html()); die;
     }
 
 }
