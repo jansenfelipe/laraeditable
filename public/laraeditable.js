45313fa18ec5d7c3e047f0a482f9b775e9c55e9f
Jansen Felipe <jansen.felipe@gmail.com>
2015-01-02 16:52:23 -0200

    info id

--- public/laraeditable.js
+++ public/laraeditable.js
@@ -1,26 +1,3 @@
-jQuery.fn.getPath = function () {
-    if (this.length != 1)
-        throw 'Requires one element.';
-    var path, node = this;
-    while (node.length) {
-        var realNode = node[0], name = realNode.localName;
-        if (!name)
-            break;
-        name = name.toLowerCase();
-
-        var parent = node.parent();
-
-        var siblings = parent.children(name);
-        if (siblings.length > 1) {
-            name += ':eq(' + siblings.index(realNode) + ')';
-        }
-
-        path = name + (path ? '>' + path : '');
-        node = parent;
-    }
-    return path;
-};
-
 $(function () {
     $('.laraeditable').each(function () {
         $(this).attr('contenteditable', 'true');
@@ -28,9 +5,9 @@ $(function () {
 
     $('.laraeditable').blur(function () {
         $.post('/laraeditable', {
-            element: $(this).getPath(),
+            id: $(this).attr('id'),
             html: $(this).html(),
-            view: $("#laraeditable").attr('view')
+            view: $(this).attr('view') 
         });
     });
 });
