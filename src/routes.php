a5cc7ebc760b61714eef34e668ba0ac5e9309e51
Jansen Felipe <jansen.felipe@gmail.com>
2015-01-02 15:13:58 -0200

    Share name view

--- src/routes.php
+++ src/routes.php
@@ -1,2 +1,5 @@
 <?php
 Route::controller('laraeditable', 'JansenFelipe\Laraeditable\Controllers\LaraeditableController');
+View::composer('*', function($view){
+    View::share('view_name', $view->getName());
+});
